d1423297a2dcef8ee746d06fda270ed0c9651eab
Sakexxpearex <bastij58@gmail.com>
2026-07-14 23:52:30 -0400

    .

--- app/Models/Cancha.php
+++ app/Models/Cancha.php
@@ -19,7 +19,34 @@ class Cancha extends Model
     public function reservas(): HasMany
     {
         return $this->hasMany(Reserva::class);
+
     }
 
+
+    
+    public function generarBloques(Carbon $fecha): array
+    {
+        $horaApertura = 9;   // 09:00
+        $horaCierre = 23;    // 23:00
+        $duracionBloqueHoras = 1;
+
+        $bloques = [];
+
+        $cursor = $fecha->copy()->setTime($horaApertura, 0);
+        $limite = $fecha->copy()->setTime($horaCierre, 0);
+
+        while ($cursor->lt($limite)) {
+            $finBloque = $cursor->copy()->addHours($duracionBloqueHoras);
+
+            $bloques[] = [
+                'inicio' => $cursor->format('H:i'),
+                'fin' => $finBloque->format('H:i'),
+            ];
+
+            $cursor = $finBloque;
+        }
+
+        return $bloques;
+    }
     
 }
