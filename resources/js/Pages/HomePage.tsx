682bc7fe90eeadf0e80068c4eaaf263ecd78f601
detossj <jorgerubilarssj@gmail.com>
2026-07-18 11:47:34 -0400

    Se cambio a la ruta que te manda el boton Admin en HomePage.tsx

--- resources/js/Pages/HomePage.tsx
+++ resources/js/Pages/HomePage.tsx
@@ -43,7 +43,7 @@ export default function HomePage({ courts, fecha }: HomePageProps) {
 
   return (
     <div className="min-h-screen bg-[#F8FAFC]">
-      <Navbar onGoAdmin={() => router.get("/admin")} />
+      <Navbar onGoAdmin={() => router.get("/login")} />
 
       <div className="pt-[72px]">
         <HeroSection />
