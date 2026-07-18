18c57e2db5404dc5d907f7ade775dc8f6dbb682b
detossj <jorgerubilarssj@gmail.com>
2026-07-18 11:24:58 -0400

    Se agrego HasFactory en Reserva.php

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Reserva extends Model
 {
-protected $fillable = [
+    use HasFactory;
+
+    protected $fillable = [
     'cancha_id',
     'cliente_nombre',
     'cliente_email',        
